64f895b64296da07fd4c75245484821b9ee662e9
DELL <krishnayadhava5@gmail.com>
2023-08-17 13:31:03 +0530

    day 12

---
+++ src/day12/Deen.java
@@ -0,0 +1,9 @@
+package day12;
+
+public interface Deen {
+
+    String doctor_name="karthik";
+
+    String give_medical_report();
+
+}
---
+++ src/day12/Doctor.java
@@ -0,0 +1,10 @@
+package day12;
+
+public interface Doctor {
+
+    String patient_name="vijay";
+
+     String buy_a_tablet();
+
+
+}
