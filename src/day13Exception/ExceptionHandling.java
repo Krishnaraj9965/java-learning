a87a2c89d5ce45d3def20939f77c3cd0d072921b
DELL <krishnayadhava5@gmail.com>
2023-08-18 11:49:44 +0530

    day 12

---
+++ src/day13Exception/ExceptionHandling.java
@@ -0,0 +1,25 @@
+package day13Exception;
+
+    public class ExceptionHandling {
+
+    public static void main(String[]  args)   {
+
+        try {
+
+        int result = divide(10,0 );
+
+        System.out.println("Result: " + result);
+    }
+
+      catch(ArithmeticException e )  {
+
+        System.out.println("An error occured: " + e. getMessage());
+
+    }
+ }
+     public static int divide(int a, int b ) {
+
+
+        return a / b;
+     }
+    }
